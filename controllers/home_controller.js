2ed402c1042c5eb0aaa9cda4f889bdce94bd9201
Keshav kumar <kumar.keshav4699@gmail.com>
2023-09-02 16:38:58 +0530

    async funtions added for some promise functions

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,24 +1,26 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = function (req, res) {
-  Post.find()
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .then((posts) => {
-      User.find()
-      .then((users)=>{
-        return res.render("home", {
-          title: "Codial|Home",
-          posts: posts,
-          all_users: users
-        });
-      })
-    })
-    .catch((err) => {});
+module.exports.home = async function (req, res) {
+  try {
+    let posts = await Post.find()
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
+      });
+
+    let users = await User.find();
+
+    return res.render("home", {
+      title: "Codial|Home",
+      posts: posts,
+      all_users: users,
+    });
+    
+  } catch (err) {
+    Console.log(`Error ${err}`);
+  }
 };
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,41 +1,31 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
 
-module.exports.create = function (req, res) {
-  Post.create({
-    content: req.body.content,
-    user: req.user.id,
-  })
-    .then((result) => {
-      console.log("new post added");
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log("error in posting new post", err);
-      res.redirect("/");
+module.exports.create = async function (req, res) {
+  try {
+    await Post.create({
+      content: req.body.content,
+      user: req.user.id,
     });
-  return;
+    return res.redirect("back");
+  } catch (err) {
+    Console.log(`Error ${err}`);
+  }
 };
 
-module.exports.destroy = function(req, res){
-  Post.findById(req.params.id)
-  .then((post)=>{
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+
     //.id means converting the object id into string and _id is object id
-    if(post.user==req.user.id){
+    if (post.user == req.user.id) {
       post.deleteOne();
-      Comment.deleteMany({post: req.params.id})
-      .then(()=>{
-        console.log("comments deleted");
-        return res.redirect('back');
-      })
-      .catch((err)=>{
-        console.log("Error in deleting comments on posts", err);
-      })
+      await Comment.deleteMany({ post: req.params.id });
     } else {
-      return res.redirect('back');
+      return res.redirect("back");
     }
-  })
-  .catch((err)=>{
-    console.log("Error in finding user for deleting comments", err);
-  })
-}
+    return res.redirect("back");
+  } catch (err) {
+    console.log(`Error ${err}`);
+  }
+};
